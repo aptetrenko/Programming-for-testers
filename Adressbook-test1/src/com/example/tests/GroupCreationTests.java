d5b896493e547f88d7c42a450fbd3c488d255eb6
aptetrenko <alexey.petrenko@cupid.com>
2014-04-07 21:42:31 +0300

    Deleting others projects

--- Adressbook-test1/src/com/example/tests/GroupCreationTests.java
+++ Adressbook-test1/src/com/example/tests/GroupCreationTests.java
@@ -1,5 +1,8 @@
 package com.example.tests;
 
+import java.util.List;
+import static org.testng.Assert.assertEquals;
+
 import org.testng.annotations.Test;
 
 
@@ -9,19 +12,32 @@ public class GroupCreationTests extends TestBase{
   public void testNonEmptyGroupCreation() throws Exception {
     app.getNavigationHelper().openMainPage();
     app.getNavigationHelper().openGroupsPage();
+    
+    // save old state
+    List<GroupData> oldList = app.getGroupHelper().getGroups();
+    
+    // actions
     app.getGroupHelper().initGroupCreation();
     GroupData group = new GroupData();
-    group.name = "nana1εσβ23";
+    group.name = "nana123";
     group.header = "header 3";
     group.footer = "footer 3";
-    
-    
-	app.getGroupHelper().fillGroupForm(group);
+    app.getGroupHelper().fillGroupForm(group);
     app.getGroupHelper().submitGroupCreation();
     app.getGroupHelper().returnToGroupsPage();
+  
+    //save new state
+    List<GroupData> newList = app.getGroupHelper().getGroups();
+    
+    //compare state
+    assertEquals(newList.size(), oldList.size()+1);
+    
+    
   }
+  
+  
 
-  @Test
+  //@Test
   public void testEmptyGroupCreation() throws Exception {
     app.getNavigationHelper().openMainPage();
     app.getNavigationHelper().openGroupsPage();
